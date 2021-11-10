92ed300a46f8c70c3b7cc53c66f69308bab15955
Vincent Thevenon <24572@student.dorset.college.ie>
2021-11-10 16:40:13 +0000

    Ajoutez des fichiers projet.

---
+++ Intrument_Library/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Intrument_Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Guitar myGuitar = new Guitar("Gibson");
+            Mandolin myMandolin = new Mandolin("Kentucky KM-140");
+            Ukele myUkele = new Ukele("Ortega");
+
+            myUkele.serialNumber = "abc123";
+
+            myGuitar.Play();
+            myMandolin.Play();
+            myUkele.Play();
+        }
+    }
+}
---
+++ Intrument_Library/instrument.library.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intrument_Library
+{
+    public abstract class Instrument
+    {
+        public string serialNumber { get; set; }
+        public string Name { get; set; }
+
+        public Instrument(string _name)
+        {
+            Name = _name;
+        }
+
+        public abstract void Play();
+    }
+
+    public class Guitar : Instrument
+    {
+        public Guitar(string _name) : base(_name)
+        {
+
+        }
+        public override void Play()
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine($"I am playing {Name} guitar");
+        }
+    }
+
+    public class Mandolin : Instrument
+    {
+        public Mandolin(string _name) : base(_name)
+        {
+
+        }
+        public override void Play()
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine($"I am playing {Name} guitar");
+        }
+    }
+
+    public class Ukele : Instrument
+    {
+        public Ukele(string _name) : base(_name)
+        {
+
+        }
+
+        public override void Play()
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine($"I am playing {Name} guitar");
+        }
+    }
+}
